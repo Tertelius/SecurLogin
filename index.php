569f45916d2650ca961b689558d54cca49673555
Tertelius <61936083+Tertelius@users.noreply.github.com>
2021-05-20 07:59:05 +0300

    Начали работу

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Авторизация</title>
+        <link rel="stylesheet" href="public/css/main.css">
+    </head>
+    <body>
+        <!--форма авторизации-->
+        
+        <form action="" method="">
+            <label>Логин: </label>
+            <input type="text" placeholder="Введите логин">
+            <label>Пароль: </label>
+            <input type="password" placeholder="Введите пароль">
+            <button>Войти</button>      
+            <p>
+                У вас нет аккаунта? - <a href="register.php">Зарегистрируйтесь</a>
+            </p>
+        </form>
+            <ol>
+            <li>раз </li>
+                <ol>
+                    <li>раз дыва</li>
+                    <li>раз три</li>
+                    <li>раз читыри.</li>
+                </ol>
+            <li>раз </li>
+                <ol>
+                    <li>раз дыва</li>
+                    <li>раз три</li>
+                    <li>раз читыри.</li>
+                </ol>
+            <li>раз </li>
+                <ol>
+                    <li>раз дыва</li>
+                    <li>раз три</li>
+                    <li>раз читыри.</li>
+                </ol>
+            </ol>
+
+        <a href="http://https://html5book.ru/html-lists/" target="_blank">Смотри ссылк</a>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
+
+<?php
+
+?>
+
+
